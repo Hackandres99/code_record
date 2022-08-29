cc9f5a5642f7ad903b3d9dad9b4ce0d472205fc7
Andrés Saavedra <andres19saavedra9castro9@gmail.com>
2022-08-29 15:32:27 -0500

    Adding link to page logo

--- layouts/base/header/logo.php
+++ layouts/base/header/logo.php
@@ -1,9 +1,12 @@
-<div class="logo_container">
-    <div class="logo">
-        <div class="circle"></div>
-        <div class="words">
-            <div class="first_line">ode</div>
-            <div class="second_line">Record</div>
+
+    <div class="logo_container">
+    <a href="?p=inicio">
+        <div class="logo">
+            <div class="circle"></div>
+            <div class="words">
+                <div class="first_line">ode</div>
+                <div class="second_line">Record</div>
+            </div>
         </div>
+        </a>
     </div>
-</div>
